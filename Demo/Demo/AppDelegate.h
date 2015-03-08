5356bfd23721cc310d541e3f6de241fd90d6aa28
袁睿 <RuiYuan@yuanruideMac-mini.local>
2015-03-08 22:03:17 +0800

    new demo

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 袁睿 on 15/3/8.
+//  Copyright (c) 2015年 袁睿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/Demo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Demo
+//
+
+//  Copyright (c) 2015年 袁睿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
